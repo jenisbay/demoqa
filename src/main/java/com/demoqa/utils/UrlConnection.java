dc05f84e2bed0c283ef70c3255551215d7fdcf05
jenisbay <j.qidirbaev@a1qa.com>
2023-10-02 15:15:50 +0500

    Added Maven Compiler Plugin Dependency 3.11.0

--- src/main/java/com/demoqa/utils/UrlConnection.java
+++ src/main/java/com/demoqa/utils/UrlConnection.java
@@ -1,27 +1,11 @@
 package com.demoqa.utils;
 
-import java.io.IOException;
-import java.net.*;
+import kong.unirest.Unirest;
 
 public class UrlConnection {
 
     public static int getResponseCode(String url) {
-        HttpURLConnection httpURLConnection = null;
-        try {
-            URI uri = new URI(url);
-            URL link = URL.of(uri, null);
-            httpURLConnection= (HttpURLConnection) link.openConnection();
-            httpURLConnection.setConnectTimeout(5000);
-            httpURLConnection.connect();
-
-            return httpURLConnection.getResponseCode();
-
-        } catch (MalformedURLException ignored) {
-
-        } catch (IOException | URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-        return 0;
+        return Unirest.get(url).asString().getStatus();
     }
 
 }
