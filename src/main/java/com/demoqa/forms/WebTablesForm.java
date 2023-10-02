ed1d01477d3e638a0a1fca9d6417583a567b04fe
jenisbay <j.qidirbaev@a1qa.com>
2023-10-02 16:40:10 +0500

    Push Commit

--- src/main/java/com/demoqa/forms/WebTablesForm.java
+++ src/main/java/com/demoqa/forms/WebTablesForm.java
@@ -17,20 +17,7 @@ public class WebTablesForm extends Form {
     }
 
     public String fetchCellText(List<String> list) {
-
-        StringBuilder formatBuilder = new StringBuilder();
-        StringBuilder resultBuilder = new StringBuilder();
-
-        for (int i = 0; i < list.toArray().length; i++) {
-            if (i == 0) {
-                formatBuilder.append(String.format("//div[contains(text(), '%s')]", list.get(i)));
-            } else {
-                formatBuilder.append(String.format("/following-sibling::div[contains(text(), '%s')]", list.get(i)));
-            }
-            resultBuilder.append(getCell(formatBuilder.toString()).getText());
-        }
-
-        return resultBuilder.toString();
+        return String.format("//div[contains(text(), '%s')]", list.get(0));
     }
 
 
